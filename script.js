3db3bd38890aebd64839a989644d2ab3a8c92f18
ActuallyVys <vys@connectdev.org>
2022-03-20 13:24:20 +0000

    added assets and fixed grammatical errors

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
   // typing text animation script
   var typed = new Typed(".typing", {
-      strings: ["privacy", "schools", "filters", "censoring", "freedom"],
+      strings: ["privacy.", "schools.", "filters.", "censorship.", "freedom."],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true
   });
 
   var typed = new Typed(".typing-2", {
-      strings: ["privacy", "schools", "filters", "censoring", "freedom"],
+      strings: ["privacy.", "schools.", "filters.", "censorship.", "freedom."],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true
